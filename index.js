6d3d1d51a7d51852378f5cd31c99315ec09e1c6a
Mannat1708 <122426796+Mannat1708@users.noreply.github.com>
2025-06-05 10:47:33 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const routes = require('./routes/routes');
+const startWorker = require('./utils/worker');
+
+const app = express();
+const PORT = 5000;
+
+app.use(bodyParser.json());
+app.use('/',routes);
+app.get('/', (req, res) => {
+  res.send('Data Ingestion API is running');
+});
+
+// Start background batch processor
+startWorker();
+
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
